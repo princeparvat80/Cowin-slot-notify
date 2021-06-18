134ab3328760967e9a6b11857c1b6b21b4fbe5ab
princeparvat80 <princeparvat80@gmail.com>
2021-06-18 13:45:42 +0530

    Folder Structure is Ready

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { PincodeComponent } from './components/pincode/pincode.component';
+import { DistrictComponent } from './components/district/district.component';
+import { MapComponent } from './components/map/map.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PincodeComponent,
+    DistrictComponent,
+    MapComponent,
+
   ],
   imports: [
     BrowserModule,

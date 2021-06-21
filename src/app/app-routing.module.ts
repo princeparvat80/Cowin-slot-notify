793bd365de2c2c194495f065910b199399e50a0c
ananya <ananyar1113@gmail.com>
2021-06-21 11:01:07 +0530

    hellomap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
+import { MapComponent } from './components/map/map.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'map',
+    component: MapComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { PincodeComponent } from './components/pincode/pincode.component';
 import { DistrictComponent } from './components/district/district.component';
 import { MapComponent } from './components/map/map.component';
+import { FormsModule } from '@angular/forms';
+import { AgmCoreModule } from '@agm/core';
+import { HttpClientModule } from '@angular/common/http';
+
 
 
 @NgModule({
@@ -20,7 +23,13 @@ import { MapComponent } from './components/map/map.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule ,
+    FormsModule,
+    AgmCoreModule.forRoot({
+      apiKey:'AIzaSyDksy6N6Z1LhNP_4RErbG7YX1sBQGq7nmk',
+      libraries: ['geometry','places']
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]

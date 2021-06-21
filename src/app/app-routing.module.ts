35cec0c8416e1391cc82979c851a6ced203ed0ae
PUNEETHRAJ V <punith967@gmail.com>
2021-06-21 11:27:15 +0530

    did some changes in bg and text

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PincodeComponent } from './components/pincode/pincode.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'pincode', component: PincodeComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

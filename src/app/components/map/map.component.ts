c57090fc1f58cf3834f80850f362ec8eedac6c40
princeparvat80 <princeparvat80@gmail.com>
2021-06-21 21:32:21 +0530

    error resolved

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -5,7 +5,7 @@ import {  MapsAPILoader } from '@agm/core';
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
-  styleUrls: ['./map.component.css']
+  styleUrls: ['./map.component.scss']
 })
 export class MapComponent implements OnInit {
   collections:any;

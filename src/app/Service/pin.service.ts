3fcb160326320c268abe02757e6e285ec4c9d5fd
Nidhi Priyadarshi <nidhip2@birlasoft.com>
2021-06-23 15:39:29 +0530

    first api done

--- src/app/Service/pin.service.ts
+++ src/app/Service/pin.service.ts
@@ -8,15 +8,14 @@ import { HttpClient } from '@angular/common/http'
 
 export class PinService {
 url:string;
-  constructor(private http:HttpClient) {this.url="https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=801503&date=23-06-2021"; }
+  constructor(private http:HttpClient) {this.url="https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin"; }
   
-  getData()
-
-  
+  getData(pinCode:string,date:string)
   {
     console.log();
     // let url="https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=110001&date=31-03-2021";
-    return this.http.get(this.url);
+    return this.http.get(this.url+ "?" + "pincode=" + pinCode + "&date=" + date);
+
     
   }
 }
--- src/app/components/pincode/pincode.component.ts
+++ src/app/components/pincode/pincode.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { CalenderService } from 'src/app/calender.service';
 import { PinService } from 'src/app/Service/pin.service';
 @Component({
@@ -11,23 +12,34 @@ export class PincodeComponent implements OnInit {
   
   result:any;
   priyadarshi:any;
-  
+  pincodeForm!: FormGroup;
   
   constructor(private code:PinService, private nidhi:CalenderService) { 
 
    
-   this.code.getData().subscribe(data=>{
-     this.result=data
-     console.warn(this.result);
-   }) 
+  // //  this.code.getData().subscribe(data=>{
+  //    this.result=data
+  //   //  console.warn(this.result);
+  //  }) 
   }
 
   ngOnInit(): void {
-    this.nidhi.getnew().subscribe((total:any)=>
-{
-this.priyadarshi=total.centers;
-console.log(this.priyadarshi);
-})
+    this.pincodeForm = new FormGroup({
+      'pincode':new FormControl()
+    })
+//     this.nidhi.getnew().subscribe((total:any)=>
+// {
+// this.priyadarshi=total.centers;
+// console.log(this.priyadarshi);
+// })
   }
-  
+  submitForm(){
+    console.log(this.pincodeForm.value)
+    const date=new Date()
+    const date1=date.getDate()+"-"+date.getMonth()+"-"+date.getFullYear()
+    this.code.getData(this.pincodeForm.controls.pincode.value,date1).subscribe(data=>{
+         this.result=data
+       console.warn(this.result);
+        }) 
+  }
 }

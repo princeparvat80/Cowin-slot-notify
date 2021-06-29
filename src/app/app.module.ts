a0b51d2c323c53214b339a4b2347d888cf3af23d
Deepali Sehgal <deepalis1@birlasoft.com>
2021-06-29 10:49:20 +0530

    district4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,10 @@ import { FormsModule } from '@angular/forms';
 import { AgmCoreModule } from '@agm/core';
 import { HttpClientModule } from '@angular/common/http';
 import {ReactiveFormsModule} from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatInputModule} from '@angular/material/input';
+import { MatNativeDateModule } from '@angular/material/core';
 
 
 
@@ -21,6 +25,7 @@ import {ReactiveFormsModule} from '@angular/forms';
     PincodeComponent,
     DistrictComponent,
     MapComponent,
+    
 
   ],
   imports: [
@@ -32,7 +37,11 @@ import {ReactiveFormsModule} from '@angular/forms';
     AgmCoreModule.forRoot({
       apiKey:'AIzaSyDksy6N6Z1LhNP_4RErbG7YX1sBQGq7nmk',
       libraries: ['geometry','places']
-    })
+    }),
+    BrowserAnimationsModule,
+    MatDatepickerModule,
+    MatInputModule,
+    MatNativeDateModule
    
   ],
   providers: [],
--- src/app/components/district/district.component.ts
+++ src/app/components/district/district.component.ts
@@ -2,7 +2,8 @@ import { StateService } from './../../state.service';
 import { InfoWindowManager } from '@agm/core';
 import { Component, OnInit } from '@angular/core';
 import { DistService } from 'src/app/dist.service';
- 
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+
 
 
 @Component({
@@ -11,18 +12,23 @@ import { DistService } from 'src/app/dist.service';
   styleUrls: ['./district.component.scss']
 })
 export class DistrictComponent implements OnInit {
- group:any;
- cluster:any;
- a:any;
- //xyz:any;
-
-  constructor(private State:StateService, private distx:StateService) { 
-    
+  group: any;
+  cluster: any;
+  a: any;
+  //xyz:any;
+  show: boolean = false;
+  searchForm!: FormGroup
+  sessionList: any;
+  data:any=[]
+  constructor(private State: StateService, private distx: DistService, private fb: FormBuilder) {
+    this.searchForm = this.fb.group({
+      districtId: new FormControl('')
+    })
   }
 
-  ngOnInit():void {
-    this.State.getstates().subscribe((display:any)=>{
-      this.group=display.states;
+  ngOnInit(): void {
+    this.State.getstates().subscribe((display: any) => {
+      this.group = display.states;
       console.log(this.group)
     })
 
@@ -32,7 +38,7 @@ export class DistrictComponent implements OnInit {
       console.log(this.xyz);
     })*/
     /*private abc:DistService*/
-    
+
     // this.distx.getdistricts().subscribe((showus:any)=>{
     //   this.cluster=showus.districts;
     //   console.log(this.cluster)
@@ -40,16 +46,28 @@ export class DistrictComponent implements OnInit {
     // this.abcde.getstateid(a).subscribe((giveus:any)=>{
     //   this.troop=giveus.
     // })
-    
-    
-    
+
+
+
   }
-  onselect(group:any){
+  onselect(group: any) {
     console.log(group)
-    this.distx.getdistricts(group.target.value).subscribe((showus:any)=>{
-      this.cluster=showus.districts;
+    this.State.getdistricts(group.target.value).subscribe((showus: any) => {
+      this.cluster = showus.districts;
       console.log(this.cluster)
-    
+
+    })
+  }
+  search() {
+    this.show = true;
+    this.data.push({date:"24-06-2021"})
+    console.log(this.searchForm.value)
+    const data = new Date();
+    const date1 = data.getDate() + '-' + data.getMonth() + '-' + data.getFullYear();
+    this.distx.getdis(this.searchForm.controls.districtId.value, date1).subscribe((res: any) => {
+
+      this.sessionList = res.sessions
+      console.log(this.sessionList, "result for service")
     })
   }
 }
